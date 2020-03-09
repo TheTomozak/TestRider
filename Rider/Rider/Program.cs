0c653cf680790ed2b193da71362f9ecef7557512
s18969 <s18969@pjwstk.edu.pl>
2020-03-10 00:42:13 +0100

    Test na rider

---
+++ Rider/Rider/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+            
+            
+        }
+    }
+}
